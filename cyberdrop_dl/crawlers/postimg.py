bade12aecc4f4563724f15e8063adaba902118d1
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-01-19 16:56:05 -0500

    fix: gallery downloads (PostImg) (#1585)

--- cyberdrop_dl/crawlers/postimg.py
+++ cyberdrop_dl/crawlers/postimg.py
@@ -11,51 +11,50 @@ from cyberdrop_dl.utils.utilities import error_handling_wrapper
 if TYPE_CHECKING:
     from cyberdrop_dl.data_structures.url_objects import ScrapeItem
 
-PRIMARY_URL = AbsoluteHttpURL("https://postimg.cc")
-DOWNLOAD_BUTTON_SELECTOR = "a[id=download]"
-API_ENTRYPOINT = AbsoluteHttpURL("https://postimg.cc/json")
-
 
 class PostImgCrawler(Crawler):
     SUPPORTED_PATHS: ClassVar[SupportedPaths] = {
-        "Album": "/gallery/...",
-        "Image": "/...",
-        "Direct links": "",
+        "Album": "/gallery/<album_id>/...",
+        "Image": "/<image_id>/...",
+        "Direct links": "i.postimg.cc/<image_id>/...",
     }
-    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
+    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://postimg.cc")
     DOMAIN: ClassVar[str] = "postimg"
     FOLDER_DOMAIN: ClassVar[str] = "PostImg"
-    OLD_DOMAINS = ("postimg.org", "postimages.org")
+    OLD_DOMAINS: ClassVar[tuple[str, ...]] = "postimg.org", "postimages.org"
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
         if "i.postimg.cc" in scrape_item.url.host:
             return await self.direct_file(scrape_item)
-        if "gallery" in scrape_item.url.parts:
-            return await self.album(scrape_item)
-        await self.image(scrape_item)
+
+        match scrape_item.url.parts[1:]:
+            case ["gallery", album_id, *_]:
+                return await self.album(scrape_item, album_id)
+            case _:
+                await self.image(scrape_item)
 
     @error_handling_wrapper
-    async def album(self, scrape_item: ScrapeItem) -> None:
-        data = {"action": "list", "album": scrape_item.url.raw_name, "page": 0}
-        title: str = ""
-        album_id = scrape_item.url.parts[2]
+    async def album(self, scrape_item: ScrapeItem, album_id: str) -> None:
+        title = self.create_title(scrape_item.url.name, album_id)
+        scrape_item.setup_as_album(title, album_id=album_id)
+
         for page in itertools.count(1):
-            json_resp = await self.request_json(
-                API_ENTRYPOINT,
+            resp = await self.request_json(
+                self.PRIMARY_URL / "json",
                 method="POST",
-                data=data | {"page": page},
+                data={
+                    "action": "list",
+                    "album": album_id,
+                    "page": page,
+                },
             )
 
-            if not title:
-                title = self.create_title(scrape_item.url.name, album_id)
-                scrape_item.setup_as_album(title, album_id=album_id)
-
-            for image in json_resp["images"]:
+            for image in resp["images"]:
                 link = self.PRIMARY_URL / image[0]
                 self.create_task(self.run(scrape_item.create_child(link)))
                 scrape_item.add_children()
 
-            if not json_resp["has_page_next"]:
+            if not resp["has_page_next"]:
                 break
 
     @error_handling_wrapper
@@ -64,8 +63,8 @@ class PostImgCrawler(Crawler):
             return
 
         soup = await self.request_soup(scrape_item.url)
-
-        link_str: str = css.select(soup, DOWNLOAD_BUTTON_SELECTOR, "href")
+        link_str = css.select(soup, "a#download", "href")
         link = self.parse_url(link_str).with_query(None)
-        filename, ext = self.get_filename_and_ext(link.name)
+        name = css.select_text(soup, ".mb-4 + h6")
+        filename, ext = self.get_filename_and_ext(name + link.suffix)
         await self.handle_file(link, scrape_item, filename, ext)
