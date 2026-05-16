380e5324e73a822e87d2564717d3a41a663d195c
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-16 21:50:30 +0000

    fix: accept URLs with any number of slashes (Fileditch) (#1836)

--- cyberdrop_dl/crawlers/fileditch.py
+++ cyberdrop_dl/crawlers/fileditch.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, ClassVar
+from typing import TYPE_CHECKING, ClassVar, override
 
 from cyberdrop_dl.crawlers.crawler import Crawler, SupportedPaths
 from cyberdrop_dl.exceptions import ScrapeError
@@ -13,26 +13,42 @@ if TYPE_CHECKING:
 DOWNLOAD_SELECTOR = 'a.btn[href*="md5="]'
 HOMEPAGE_CATCHALL_FILE = "/s21/FHVZKQyAZlIsrneDAsp.jpeg"
 
-PRIMARY_URL = AbsoluteHttpURL("https://fileditchfiles.me/")
-
 
 class FileditchCrawler(Crawler):
-    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {"Direct links": ""}
-    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = PRIMARY_URL
+    SUPPORTED_PATHS: ClassVar[SupportedPaths] = {
+        "File": "/file.php?f=<file_id>",
+    }
+    PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://fileditchfiles.me/")
     DOMAIN: ClassVar[str] = "fileditch"
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
-        if scrape_item.url.path != "/file.php":
-            # Some old files are only direct linkable
-            return await self.direct_file(scrape_item)
-        return await self.file(scrape_item)
+        match scrape_item.url.parts[1:]:
+            case [*_, "file.php"]:
+                return await self.file(scrape_item)
+            case _:
+                raise ValueError
+
+    @classmethod
+    @override
+    def transform_url(cls, url: AbsoluteHttpURL) -> AbsoluteHttpURL:
+        url = super().transform_url(url)
+        if url.name == "file.php" and (file_id := url.query.get("f")):
+            # Their servers accept any number of empty parts before the file
+            # Remove them to get a canonical URL
+            # https:/fileditchfiles.me////////file.php?f=/b70/1234 -> https:/fileditchfiles.me/file.php?f=/b70/1234
+            return (url.origin() / "file.php").with_query(f=file_id)
+        return url
 
     @error_handling_wrapper
     async def file(self, scrape_item: ScrapeItem) -> None:
+        if await self.check_complete_from_referer(scrape_item.url):
+            return
+
         soup = await self.request_soup(scrape_item.url)
         link_str: str = css.select(soup, DOWNLOAD_SELECTOR, "href")
         link = self.parse_url(link_str)
         if link.path == HOMEPAGE_CATCHALL_FILE:
             raise ScrapeError(422)
+
         filename, ext = self.get_filename_and_ext(link.name)
         await self.handle_file(link, scrape_item, filename, ext)
--- tests/crawlers/test_cases/fileditch.py
+++ tests/crawlers/test_cases/fileditch.py
@@ -4,12 +4,24 @@ TEST_CASES = [
         "url": "https://fileditchfiles.me/file.php?f=/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
         "results": [
             {
-                "url": "re:https://thegumonmyshoe.me/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
+                "url": "re:thegumonmyshoe.me/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
                 "filename": "FrmLzfLKUHBWDTQfqaTZ.mp4",
                 "referer": "https://fileditchfiles.me/file.php?f=/b71/FrmLzfLKUHBWDTQfqaTZ.mp4",
                 "download_folder": "re:Loose Files (Fileditch)",
                 "uploaded_at": None,
             }
         ],
-    }
+    },
+    {
+        "url": "https://fileditchfiles.me//file.php?f=/b70/jRuBeGZlRoBWPurUARg.mp4",
+        "description": "multiple slashes on URL",
+        "results": [
+            {
+                "url": "re:thegumonmyshoe.me/b70/jRuBeGZlRoBWPurUARg.mp4",
+                "referer": "https://fileditchfiles.me/file.php?f=/b70/jRuBeGZlRoBWPurUARg.mp4",
+                "download_folder": "re:Loose Files (Fileditch)",
+                "uploaded_at": None,
+            }
+        ],
+    },
 ]
