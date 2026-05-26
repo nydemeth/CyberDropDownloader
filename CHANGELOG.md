359ad8b6c5435a4cb30fe3ddd17c3fcc19254baf
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-26 05:05:22 +0000

    feat: add whyp.it support (#1905)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -31,6 +31,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - YTboob support
 - Rutube support
 - Acast support
+- Whyp.it support
 - Soundgasm support
 
 ### Changed
---
+++ tests/crawlers/test_cases/whyp_it.py
@@ -0,0 +1,82 @@
+DOMAIN = "whyp.it"
+TEST_CASES = [
+    {
+        "url": "https://whyp.it/tracks/344383/reading-your-shy-besties-mind?token=WBPWv",
+        "description": "Private track (requires the ?token param to access)",
+        "results": [
+            {
+                "url": "re:https://cdn.whyp.it/7777d4c0-0b06-4500-89e0-f0d960b0a4a4.mp3?token",
+                "filename": "Reading Your Shy Besties Mind [344383].mp3",
+                "debrid_link": None,
+                "original_filename": "Reading Your Shy Bestie's Mind",
+                "referer": "https://whyp.it/tracks/344383/reading-your-shy-besties-mind?token=WBPWv",
+                "album_id": None,
+                "uploaded_at": 1777422482,
+                "download_folder": "re:beegeewanders (Whyp.it)",
+            }
+        ],
+        "count": 1,
+    },
+    {
+        "url": "https://whyp.it/users/1/brad",
+        "results": [
+            {
+                "url": "re:https://cdn.whyp.it/5e9de576-f33a-40ea-bd43-1693a568a6a0.mp3",
+                "filename": "Fallout 3 Intro Remake [13721].mp3",
+                "debrid_link": None,
+                "original_filename": "Fallout 3 Intro Remake",
+                "referer": "https://whyp.it/tracks/13721",
+                "album_id": None,
+                "uploaded_at": 1764003590,
+                "download_folder": "re:Brad (Whyp.it)",
+            },
+            {
+                "url": "re:https://cdn.whyp.it/0d7a196b-3e1a-4510-a4a4-6189c56ecb27.flac",
+                "filename": "Jason Waters - Heart of the Throne [324260].flac",
+                "debrid_link": None,
+                "original_filename": "Jason Waters - Heart of the Throne",
+                "referer": "https://whyp.it/tracks/324260",
+                "album_id": None,
+                "uploaded_at": 1763408657,
+                "download_folder": "re:Brad (Whyp.it)",
+            },
+            {
+                "url": "re:https://cdn.whyp.it/3d134d07-7c55-4a6b-b321-56ce90ee1fc8.flac",
+                "filename": "Example Track [18337].flac",
+                "debrid_link": None,
+                "original_filename": "Example Track",
+                "referer": "https://whyp.it/tracks/18337",
+                "album_id": None,
+                "uploaded_at": 1643216583,
+                "download_folder": "re:Brad (Whyp.it)",
+            },
+        ],
+        "count": 3,
+    },
+    {
+        "url": "https://whyp.it/collections/1/example-collection",
+        "results": [
+            {
+                "url": "re:https://cdn.whyp.it/3d134d07-7c55-4a6b-b321-56ce90ee1fc8.flac?token=",
+                "filename": "Example Track [18337].flac",
+                "debrid_link": None,
+                "original_filename": "Example Track",
+                "referer": "https://whyp.it/tracks/18337",
+                "album_id": None,
+                "uploaded_at": 1708365679,
+                "download_folder": "re:Brad (Whyp.it)/Example Collection",
+            },
+            {
+                "url": "re:https://cdn.whyp.it/0d7a196b-3e1a-4510-a4a4-6189c56ecb27.flac?token=",
+                "filename": "Jason Waters - Heart of the Throne [324260].flac",
+                "debrid_link": None,
+                "original_filename": "Jason Waters - Heart of the Throne",
+                "referer": "https://whyp.it/tracks/324260",
+                "album_id": None,
+                "uploaded_at": 1763408701,
+                "download_folder": "re:Brad (Whyp.it)/Example Collection",
+            },
+        ],
+        "count": 2,
+    },
+]
