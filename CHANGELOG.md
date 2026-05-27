adc0e79cf1918ad951e1ad2ffcf64fb582508024
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-27 03:24:40 +0000

    feat: add livestreamfails support (#1909)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -34,6 +34,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Whyp.it support
 - Soundgasm support
 - Clyp.it support
+- Livestreamfails support
 
 ### Changed
 
---
+++ tests/crawlers/test_cases/livestreamfails.py
@@ -0,0 +1,45 @@
+DOMAIN = "livestreamfails.com"
+TEST_CASES = [
+    {
+        "url": "https://livestreamfails.com/clip/179468",
+        "results": [
+            {
+                "url": "https://livestreamfails-video-prod.b-cdn.net/video/7c1c6131244daeaeaa2aee88e5dff03c54944901.mp4",
+                "filename": "Ariathome runs into 2025 US Beatbox Champion (BizKit) [179468].mp4",
+                "debrid_link": None,
+                "original_filename": "Ariathome runs into 2025 US Beatbox Champion (BizKit)",
+                "referer": "https://livestreamfails.com/clip/179468",
+                "album_id": None,
+                "uploaded_at": 1775626544,
+                "download_folder": "re:ARIatHOME (Livestreamfails.com)",
+            }
+        ],
+        "count": 1,
+    },
+    {
+        "url": "https://livestreamfails.com/streamer/11261",
+        "results": [
+            {
+                "url": "https://livestreamfails-video-prod.b-cdn.net/video/7b75a6775160fa346757cf6aca62aca2180ccfe3.mp4",
+                "filename": "The most Insane Ice Bath [178966].mp4",
+                "debrid_link": None,
+                "original_filename": "The most Insane Ice Bath",
+                "referer": "https://livestreamfails.com/clip/178966",
+                "album_id": None,
+                "uploaded_at": 1770664483,
+                "download_folder": "re:KattyCore (Livestreamfails.com)",
+            },
+            {
+                "url": "https://livestreamfails-video-prod.b-cdn.net/video/d0a0c77a026cf5df9624c70cf4ee6b7ab0e601c0.mp4",
+                "filename": "Huge Snowman Reveal [178623].mp4",
+                "debrid_link": None,
+                "original_filename": "Huge Snowman Reveal",
+                "referer": "https://livestreamfails.com/clip/178623",
+                "album_id": None,
+                "uploaded_at": 1767287443,
+                "download_folder": "re:KattyCore (Livestreamfails.com)",
+            },
+        ],
+        "count": 2,
+    },
+]
