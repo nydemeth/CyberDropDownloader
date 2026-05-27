073321f4e5d86a44964aff126dda48743b322561
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-05-27 03:51:31 +0000

    feat: add support for fatbox proxy (Catbox) (#1911)

---
+++ tests/crawlers/test_cases/catbox_moe.py
@@ -0,0 +1,35 @@
+DOMAIN = "catbox.moe"
+TEST_CASES = [
+    {
+        "url": "https://files.catbox.moe/ey8s2h.zip",
+        "results": [
+            {
+                "url": "https://files.catbox.moe/ey8s2h.zip",
+                "filename": "ey8s2h.zip",
+                "debrid_link": None,
+                "original_filename": "ey8s2h.zip",
+                "referer": "https://files.catbox.moe/ey8s2h.zip",
+                "album_id": None,
+                "uploaded_at": None,
+                "download_folder": "re:Loose Files (Catbox)",
+            }
+        ],
+        "count": 1,
+    },
+    {
+        "url": "https://files.fatbox.moe/ey8s2h.zip",
+        "results": [
+            {
+                "url": "https://files.fatbox.moe/ey8s2h.zip",
+                "filename": "ey8s2h.zip",
+                "debrid_link": None,
+                "original_filename": "ey8s2h.zip",
+                "referer": "https://files.fatbox.moe/ey8s2h.zip",
+                "album_id": None,
+                "uploaded_at": None,
+                "download_folder": "re:Loose Files (Catbox)",
+            }
+        ],
+        "count": 1,
+    },
+]
