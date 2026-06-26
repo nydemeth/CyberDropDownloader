cc41bda0febf0f1a2afe114c08bf8421d6917811
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-06-26 17:25:58 -0500

    refactor: stats colors

--- cyberdrop_dl/stats.py
+++ cyberdrop_dl/stats.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import functools
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, final
 
 from cyberdrop_dl.logs import log_spacer
 from cyberdrop_dl.progress.dedupe import DedupeStats
@@ -12,12 +12,24 @@ from cyberdrop_dl.progress.scraping.files import FileStats
 from cyberdrop_dl.progress.sorting import SortStats
 
 if TYPE_CHECKING:
-    from collections.abc import Sequence
+    from collections.abc import Mapping, Sequence
 
     from cyberdrop_dl.progress.scraping.errors import UIError
 
 
 logger = logging.getLogger(__name__)
+
+
+def color(name: str) -> Mapping[str, str]:
+    return {"color": name}
+
+
+@final
+class Color:
+    RED = color("red")
+    GREEN = color("green")
+    CYAN = color("cyan")
+    YELLOW = color("yellow")
 
 
 @functools.singledispatch
@@ -28,7 +40,7 @@ def print(stats: object) -> None:  # noqa: A001
 @print.register
 def _(stats: SortStats) -> None:
     log_spacer()
-    logger.info("Sort Stats:", extra={"color": "cyan"})
+    logger.info("Sort Stats:", extra=Color.CYAN)
     logger.info(f"  Audios: {stats.audios:,}")
     logger.info(f"  Images: {stats.images:,}")
     logger.info(f"  Videos: {stats.videos:,}")
@@ -38,7 +50,7 @@ def _(stats: SortStats) -> None:
 @print.register
 def _(stats: HashingStats) -> None:
     log_spacer()
-    logger.info("Checksum Stats:", extra={"color": "cyan"})
+    logger.info("Checksum Stats:", extra=Color.CYAN)
     logger.info(f"  Newly hashed: {stats.new_hashed:,} files")
     logger.info(f"  Previously hashed: {stats.prev_hashed:,} files")
 
@@ -46,27 +58,28 @@ def _(stats: HashingStats) -> None:
 @print.register
 def _(stats: DedupeStats) -> None:
     log_spacer()
-    logger.info("Dedupe Stats:", extra={"color": "cyan"})
+    logger.info("Dedupe Stats:", extra=Color.CYAN)
     logger.info(f"  Deleted (duplicates of previous downloads): {stats.deleted:,} files")
-    logger.info(f"  Errors: {stats.total - stats.deleted:,} files")
+    errors = stats.total - stats.deleted
+    logger.info(f"  Errors: {errors:,} files", extra=Color.RED if errors else None)
 
 
 @print.register
 def _(stats: FileStats) -> None:
     log_spacer()
-    logger.info("Download Stats:", extra={"color": "cyan"})
+    logger.info("Download Stats:", extra=Color.CYAN)
     logger.info(f"  Downloaded: {stats.completed:,} files")
     logger.info(f"  Skipped (by config): {stats.skipped:,} files")
     logger.info(f"  Skipped (previously downloaded): {stats.previously_completed:,} files")
-    logger.info(f"  Failed: {stats.failed:,} files")
+    logger.info(f"  Failed: {stats.failed:,} files", extra=Color.RED if stats.failed else None)
 
 
 @print.register
 def _(stats: ScrapeErrorsPanel) -> None:
     log_spacer()
-    logger.info("Unsupported URLs Stats:", extra={"color": "cyan"})
+    logger.info("Unsupported URLs Stats:", extra=Color.CYAN)
     logger.info(f"  Sent to Jdownloader: {stats.sent_to_jdownloader:,}")
-    logger.info(f"  Skipped: {stats.skipped:,}")
+    logger.info(f"  Skipped: {stats.skipped:,}", extra=Color.YELLOW if stats.skipped else None)
 
 
 def print_errors(scrape_errors: Sequence[UIError], download_errors: Sequence[UIError]) -> None:
@@ -82,10 +95,10 @@ def print_errors(scrape_errors: Sequence[UIError], download_errors: Sequence[UIE
         ("Download Errors:", download_errors),
     ):
         log_spacer()
-        logger.info(title, extra={"color": "cyan"})
+        logger.info(title, extra=Color.CYAN)
         if not errors:
-            logger.info(f"  {'None':>{padding}}", extra={"color": "green"})
+            logger.info(f"  {'None':>{padding}}")
             continue
 
         for error in errors:
-            logger.info(f"  {error.format(padding)}", extra={"color": "red"})
+            logger.info(f"  {error.format(padding)}", extra=Color.RED)
