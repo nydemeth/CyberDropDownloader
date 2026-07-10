b3d9bd5e00ab86deb4ea8e2b76cee6f21f5e9459
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-10 00:39:22 +0000

    feat: add daftporn support (#2019)

---
+++ tests/crawlers/test_cases/daftporn.py
@@ -0,0 +1,19 @@
+DOMAIN = "daftporn"
+TEST_CASES = [
+    {
+        "url": "https://www.daftporn.com/extreme-videos/Bar-waitress-made-a-new-friend-last-night.php",
+        "results": [
+            {
+                "url": "https://www.daftporn.com/movies/bar_26_05_20.mp4",
+                "filename": "Bar waitress made a new friend last night.mp4",
+                "debrid_url": None,
+                "original_filename": "Bar waitress made a new friend last night",
+                "referer": "https://www.daftporn.com/extreme-videos/Bar-waitress-made-a-new-friend-last-night.php",
+                "album_id": None,
+                "uploaded_at": None,
+                "download_folder": "re:Loose Files (Daftporn)",
+            }
+        ],
+        "count": 1,
+    }
+]
