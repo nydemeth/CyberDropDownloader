be28937f7a7eacab817c49f1549e1cd7f8775a89
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-07-10 00:45:06 +0000

    feat: add pillowcase support (#2021)

---
+++ tests/crawlers/test_cases/pillowcase.py
@@ -0,0 +1,29 @@
+DOMAIN = "pillowcase"
+TEST_CASES = [
+    {
+        "url": "https://pillows.su/f/19ebe13944c4d628277e09c3eed52d09",
+        "results": [
+            {
+                "url": "https://api.pillows.su/api/download/19ebe13944c4d628277e09c3eed52d09",
+                "filename": "File Aug 26, 6 16 32 PM [19ebe13944c4d628277e09c3eed52d09].mp3",
+                "debrid_url": None,
+                "original_filename": "File Aug 26, 6 16 32 PM.mp3",
+                "referer": "https://pillows.su/f/19ebe13944c4d628277e09c3eed52d09",
+                "album_id": None,
+                "uploaded_at": None,
+                "download_folder": "re:Loose Files (PillowCase)",
+            },
+        ],
+        "count": 1,
+    },
+    {
+        "url": "https://pillowcase.su/f/19ebe13944c4d628277e09c3eed52d09",
+        "results": [
+            {
+                "url": "https://api.pillows.su/api/download/19ebe13944c4d628277e09c3eed52d09",
+                "filename": "File Aug 26, 6 16 32 PM [19ebe13944c4d628277e09c3eed52d09].mp3",
+                "referer": "https://pillows.su/f/19ebe13944c4d628277e09c3eed52d09",
+            },
+        ],
+    },
+]
