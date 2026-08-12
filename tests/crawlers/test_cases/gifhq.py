9fa1da3c4c0c1c05129a3a580e7d0288bfc94a44
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-08-12 00:08:48 -0500

    feat: add gifhq support

---
+++ tests/crawlers/test_cases/gifhq.py
@@ -0,0 +1,35 @@
+DOMAIN = "gifhq"
+TEST_CASES = [
+    {
+        "url": "https://gifhq.com/post/41337570",
+        "results": [
+            {
+                "url": "https://i.imgur.com/hYG9m01.jpg",
+                "filename": "hYG9m01.jpg",
+                "debrid_url": None,
+                "original_filename": "hYG9m01.jpg",
+                "referer": "https://i.imgur.com/hYG9m01.jpg",
+                "album_id": None,
+                "uploaded_at": None,
+                "download_folder": "re:Loose Files (Imgur)",
+            }
+        ],
+        "count": 1,
+    },
+    {
+        "url": "https://gifhq.com/post/201646890",
+        "results": [
+            {
+                "url": "https://media.redgifs.com/BossyAdorableNauplius.mp4",
+                "filename": "BossyAdorableNauplius.mp4",
+                "debrid_url": None,
+                "original_filename": "BossyAdorableNauplius.mp4",
+                "referer": "https://www.redgifs.com/watch/bossyadorablenauplius",
+                "album_id": None,
+                "uploaded_at": 1786482792,
+                "download_folder": "re:Loose Files (RedGifs)",
+            }
+        ],
+        "count": 1,
+    },
+]
