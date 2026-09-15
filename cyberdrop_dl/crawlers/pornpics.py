a1ad89ed2083e8bca7540f5dc126b1293aa8c61c
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-09-15 14:21:37 -0500

    refactor: rework crawler (Pornpics)
    fix pornstars, channels and search results
    add support for videos

--- cyberdrop_dl/crawlers/pornpics.py
+++ cyberdrop_dl/crawlers/pornpics.py
@@ -1,33 +1,35 @@
 from __future__ import annotations
 
+import asyncio
+import base64
+import dataclasses
 import itertools
 from typing import TYPE_CHECKING, ClassVar
 
-from cyberdrop_dl.crawlers.crawler import Crawler, SupportedPaths
+from cyberdrop_dl import aio
+from cyberdrop_dl.crawlers.crawler import Crawler, SupportedPaths, URLConfig
 from cyberdrop_dl.url_objects import AbsoluteHttpURL
-from cyberdrop_dl.utils import css
+from cyberdrop_dl.utils import css, extr_text
 from cyberdrop_dl.utils.errors import error_handling_wrapper
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncGenerator
+    from collections.abc import AsyncGenerator, Iterable
 
-    from bs4 import BeautifulSoup
+    import bs4
 
     from cyberdrop_dl.url_objects import ScrapeItem
 
-COLLECTION_PARTS = "search", "channel", "pornstar", "tag", "category"
-IMAGE_SELECTOR = "div#main a.rel-link"
-BASE_HOST: str = "pornpics.com"
 
-
+@URLConfig(allow_empty_path=True)
 class PornPicsCrawler(Crawler):
     SUPPORTED_PATHS: ClassVar[SupportedPaths] = {
-        "Categories": "/categories/....",
-        "Channels": "/channels/...",
-        "Gallery": "/galleries/...",
-        "Pornstars": "/pornstars/...",
+        "Categories": "/<category>/",
+        "Channels": "/channels/<name>",
+        "Gallery": "/galleries/<name>-<gallery_id>",
+        "Pornstars": "/pornstars/<name>",
+        "Video preview": "/videos/<video_id>",
+        "Tags": "/tags/<name>",
         "Search": "/?q=<query>",
-        "Tags": "/tags/...",
         "Direct links": "",
     }
     PRIMARY_URL: ClassVar[AbsoluteHttpURL] = AbsoluteHttpURL("https://pornpics.com")
@@ -35,92 +37,113 @@ class PornPicsCrawler(Crawler):
     FOLDER_DOMAIN: ClassVar[str] = "PornPics"
 
     async def fetch(self, scrape_item: ScrapeItem) -> None:
-        parts_limit = 2 if scrape_item.url.name else 3
-        multi_part = len(scrape_item.url.parts) > parts_limit
-        collection_part = next((p.removesuffix("s") for p in COLLECTION_PARTS if p in scrape_item.url.parts), None)
-        if is_cdn(scrape_item.url):
-            return await self.image(scrape_item)
-        if "galleries" in scrape_item.url.parts and multi_part:
-            return await self.gallery(scrape_item)
-        if multi_part and collection_part:
-            return await self.collection(scrape_item, collection_part)
-        if len(scrape_item.url.parts) == parts_limit:
-            return await self.collection(scrape_item, "category")
-        if scrape_item.url.query.get("q"):
-            return await self.collection(scrape_item, "search")
-        raise ValueError
+        match scrape_item.url.parts[1:]:
+            case ["galleries", slug, *_]:
+                gallery_id = slug.rpartition("-")[-1]
+                await self.gallery(scrape_item, gallery_id)
+            case ["tag" | "tags" | "category" as col, _, *_]:
+                await self.collection(scrape_item, col)
+            case ["search" | "channel" | "channels" | "pornstar" | "pornstars" as col, query, *_]:
+                await self.collection(scrape_item, col, query)
+            case ["videos", video_id]:
+                await self.video_preview(scrape_item, video_id)
+            case [_, _, *_, album_id, name] if name and self.is_subdomain(scrape_item.url):
+                scrape_item.album_id = album_id
+                await self.direct_file(scrape_item)
+            case [] if query := scrape_item.url.query.get("q"):
+                await self.collection(scrape_item, "search", query)
+            case [_]:
+                await self.collection(scrape_item, "category")
+            case _:
+                raise ValueError
 
-    @error_handling_wrapper
-    async def collection(self, scrape_item: ScrapeItem, collection_type: str) -> None:
-        assert collection_type in COLLECTION_PARTS
+    async def _collection_pager(self, url: AbsoluteHttpURL) -> AsyncGenerator[Iterable[tuple[str, AbsoluteHttpURL]]]:
+        limit: int = 20  # This is hardcoded server side
+        page_url = url.without_query_params("offset").update_query(limit=limit)
 
-        def update_scrape_item(soup: BeautifulSoup) -> None:
-            selector = "h2" if collection_type == "channel" else "h1"
-            title = css.select_text(soup, selector).removesuffix(" Nude Pics").removesuffix(" Porn Pics")
-            title = self.create_title(f"{title} [{collection_type}]")
-            scrape_item.setup_as_profile(title)
-
-        async for soup, items in self._pager(scrape_item):
-            if soup:
-                update_scrape_item(soup)
-
-            for link in items:
-                new_scrape_item = scrape_item.create_child(link)
-                self.create_task(self.run(new_scrape_item, check_referer=True))
-                scrape_item.add_children()
-
-    @error_handling_wrapper
-    async def gallery(self, scrape_item: ScrapeItem) -> None:
-        gallery_base = scrape_item.url.name or scrape_item.url.parent.name
-        gallery_id = gallery_base.rsplit("-", 1)[-1]
-        results = await self.get_album_results(gallery_id)
-
-        soup = await self.request_soup(scrape_item.url)
-
-        scrape_item.url = self.PRIMARY_URL / "galleries" / gallery_id  # canonical URL
-        title = css.select_text(soup, "h1")
-        title = self.create_title(title, gallery_id)
-        scrape_item.setup_as_album(title, album_id=gallery_id)
-
-        for new_scrape_item in self.iter_children(scrape_item, soup, IMAGE_SELECTOR):
-            if not self.check_album_results(new_scrape_item.url, results):
-                filename, ext = self.get_filename_and_ext(new_scrape_item.url.name)
-                await self.handle_file(new_scrape_item.url, new_scrape_item, filename, ext)
-
-    @error_handling_wrapper
-    async def image(self, scrape_item: ScrapeItem) -> None:
-        scrape_item.album_id = scrape_item.url.parts[-2]
-        await self.direct_file(scrape_item)
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~``
-
-    async def _pager(
-        self, scrape_item: ScrapeItem
-    ) -> AsyncGenerator[tuple[BeautifulSoup | None, tuple[AbsoluteHttpURL, ...]]]:
-        """Generator of website pages."""
-        limit = 20
-        page_url = scrape_item.url.without_query_params("limit", "offset").with_query(limit=limit)
-        init_offset = int(scrape_item.url.query.get("offset") or 0)
-
-        async def get_items(offset: int) -> tuple[BeautifulSoup | None, tuple[AbsoluteHttpURL, ...]]:
-            current_page = page_url.update_query(offset=offset)
-            if not offset:  # offset == 0 does not return JSON
-                soup = await self.request_soup(current_page)
-                items = soup.select(IMAGE_SELECTOR)
-                return soup, tuple(self.parse_url(css.attr(image, "href")) for image in items)
-
-            # The response is JSON but the "content-type" is wrong
-            async with self.request(current_page) as resp:
-                json_resp = await resp.json(content_type=False)
-            return None, tuple(self.parse_url(g["g_url"]) for g in json_resp)
+        # We intentionally skip the first result cause offset 0 return HTML instead of JSON
+        init_offset = min(int(url.query.get("offset") or 1), 1)
 
         for offset in itertools.count(init_offset, limit):
-            soup, items = await get_items(offset)
-            yield soup, items
-            if len(items) < limit:
+            async with self.request(page_url.update_query(offset=offset)) as resp:
+                resp = await resp.json(content_type=False)
+
+            yield ((str(gallery["gid"]), self.parse_url(gallery["g_url"])) for gallery in resp)
+
+            if len(resp) < limit:
                 break
 
+    @error_handling_wrapper
+    async def collection(self, scrape_item: ScrapeItem, kind: str, query: str | None = None) -> None:
+        soup = await self.request_soup(scrape_item.url.without_query_params("limit", "offset"))
+        name = (
+            css.select_text(soup, ".entity-card-title__name, .page-title-section h1")
+            .removesuffix(" Nude Pics")
+            .removesuffix(" Porn Pics")
+            .strip()
+        )
+        scrape_item.setup_as_profile(self.create_title(f"{name} [{kind.removesuffix('s')}]"))
+        search_url = (
+            (self.PRIMARY_URL / "search/srch.php").with_query(q=query.replace("-", " ")) if query else scrape_item.url
+        )
 
-def is_cdn(url: AbsoluteHttpURL) -> bool:
-    url_host: str = url.host.removeprefix("www.")
-    return len(url_host.split(".")) > len(BASE_HOST.split("."))
+        async for galleries in self._collection_pager(search_url):
+            async with self.new_task_group() as tg:
+                for gallery_id, gallery_url in galleries:
+                    if self.was_scrapped_before(gallery_url):
+                        continue
+                    new_item = scrape_item.create_child(gallery_url)
+                    tg.create_task(self.gallery(new_item, gallery_id))
+                    scrape_item.add_children()
+
+    @error_handling_wrapper
+    async def gallery(self, scrape_item: ScrapeItem, gallery_id: str) -> None:
+        soup, completed = await aio.gather(self.request_soup(scrape_item.url), self.get_completed_by_album(gallery_id))
+        name = css.select_text(soup, ".gallery-title h1")
+        title = self.create_title(name, gallery_id)
+        scrape_item.setup_as_album(title, album_id=gallery_id)
+
+        async with self.new_task_group() as tg:
+            for url in self.iter_urls(soup, "div#main a.rel-link"):
+                if url in completed:
+                    continue
+
+                tg.create_task(self.direct_file(scrape_item, url))
+
+    @error_handling_wrapper
+    async def video_preview(self, scrape_item: ScrapeItem, video_id: str) -> None:
+        async with self.request(scrape_item.url) as resp:
+            soup = await resp.soup()
+            html = await resp.text()
+
+        video = await asyncio.to_thread(_extr_video, soup, html)
+        _, ext = self.get_filename_and_ext(video.src.name)
+        await self.handle_file(
+            scrape_item.url,
+            scrape_item,
+            video.name,
+            ext,
+            custom_filename=self.create_custom_filename(video.name, ext, file_id=video_id),
+            thumbnail=video.thumb,
+            debrid_link=video.src,
+        )
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class PreviewVideo:
+    name: str
+    src: AbsoluteHttpURL
+    thumb: AbsoluteHttpURL
+    uploaded: float
+
+
+def _extr_video(soup: bs4.Tag, html: str) -> PreviewVideo:
+    pp_link = extr_text(html, "var P_LINK =", ";").strip("'").removeprefix("dd/")
+    return PreviewVideo(
+        src=PornPicsCrawler.parse_url(base64.b64decode(pp_link.encode()).decode()),
+        uploaded=Crawler.parse_iso_date(
+            css.select_text(soup, ".gallery-info__item:-soup-contains('Added on:') .info-rate")
+        ),
+        name=css.select_text(soup, ".title-section h1"),
+        thumb=PornPicsCrawler.parse_url(css.select(soup, "video[poster]", "poster")),
+    )
